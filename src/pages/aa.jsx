8663ceaf0720043d61717cb7ca75b806d35e4a48
rbnlngs <langiusruben@gmail.com>
2024-10-14 16:45:40 +0200

    Update aa test

---
+++ src/pages/aa.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import {Text, Container, Link } from 'theme-ui'
+import Layout from '../components/Layout'
+import { Header, HeaderName, HeaderTitle } from '../components/Header'
+
+const Details = (props) => <Text sx={{ fontSize: 0 }} {...props} />
+
+const UsesPage = () => (
+  <Layout>
+    <Helmet>
+      <title>AA-test</title>
+    </Helmet>
+
+    <Header>
+      <HeaderName>Wishlist</HeaderName>
+
+      <HeaderTitle>AA test</HeaderTitle>
+    </Header>
+
+    <Container mt={4}>
+      <iframe
+        id="inlineFrameExample"
+        title="Inline Frame Example"
+        width="300"
+        height="200"
+        style="margin: 0 auto"
+        src="https://local-first-bias-detection.s3.eu-central-1.amazonaws.com/index.html">
+      </iframe>
+    </Container>
+  </Layout>
+)
+
+export default UsesPage
